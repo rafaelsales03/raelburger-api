d162d4cf37637f154f1586e95821fc56a7d33c30
rafaelsales03 <elrafael76234@gmail.com>
2024-07-04 14:05:05 -0300

    first commit

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+    secret: 'ba3cf06b44a74cc654067b50cc4a00cb',
+    expiresIn: '5d'
+}
---
+++ src/server.js
@@ -0,0 +1,3 @@
+import app from './app';
+
+app.listen(3001, () => console.log('Servnger is runni at port 3001...'));
